f4e7324f8ffa381646c820a46f02aa02d15cf4d6
apwbs <edoardo.marangone@uniroma1.it>
2023-01-06 15:57:14 +0100

    attribute certifier database interaction

--- impl/attribute_certifier.py
+++ impl/attribute_certifier.py
@@ -2,6 +2,9 @@ import json
 from decouple import config
 import ipfshttpclient
 import os
+import io
+
+api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
 
 app_id_box = config('APPLICATION_ID_BOX')
 app_id_certifier = config('APPLICATION_ID_CERTIFIER')
@@ -28,16 +31,24 @@ def generate_attributes():
                                  'MECHANICS@TU']
     }
 
-    api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
-    dict_users_dumped = json.dumps(dict_users)
-    name_file = 'files/users_attributes_' + str(process_instance_id) + '.txt'
-    # there is no need to save the file locally
-    with open(name_file, 'w') as ua:
-        ua.write('"process_instance_id": ' + str(process_instance_id) + '\n')
-        ua.write(dict_users_dumped)
-    new_file = api.add(name_file)
-    hash_file = new_file['Hash']
-    print(f'ipfs hash: {hash_file}')
+    f = io.StringIO()
+    f.write(str(dict_users))
+    f.seek(0)
+
+    file_to_str = f.read()
+
+    hash_file = api.add_json(file_to_str)
+    print(hash_file)
+
+    # dict_users_dumped = json.dumps(dict_users)
+    # name_file = 'files/users_attributes_' + str(process_instance_id) + '.txt'
+    # # there is no need to save the file locally
+    # with open(name_file, 'w') as ua:
+    #     ua.write('"process_instance_id": ' + str(process_instance_id) + '\n')
+    #     ua.write(dict_users_dumped)
+    # new_file = api.add(name_file)
+    # hash_file = new_file['Hash']
+    # print(f'ipfs hash: {hash_file}')
 
     print(os.system('python3.11 blockchain/AttributeCertifierContract/AttributeCertifierContractMain.py %s %s %s %s' %
                     (certifier_private_key, app_id_certifier, process_instance_id, hash_file)))

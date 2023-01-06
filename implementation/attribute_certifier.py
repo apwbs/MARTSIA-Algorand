74944071387a2d365acd8f591c4c5c4eaa269b89
apwbs <edoardo.marangone@uniroma1.it>
2023-01-06 21:42:33 +0100

    interaction with database started

--- implementation/attribute_certifier.py
+++ implementation/attribute_certifier.py
@@ -2,6 +2,10 @@ import json
 from decouple import config
 import ipfshttpclient
 import os
+import io
+import sqlite3
+
+api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
 
 app_id_box = config('APPLICATION_ID_BOX')
 app_id_certifier = config('APPLICATION_ID_CERTIFIER')
@@ -10,6 +14,10 @@ certifier_private_key = config('CERTIFIER_PRIVATEKEY')
 manufacturer_address = config('READER_ADDRESS_MANUFACTURER')
 supplier1_address = config('READER_ADDRESS_SUPPLIER1')
 supplier2_address = config('READER_ADDRESS_SUPPLIER2')
+
+# Connection to SQLite3 attribute_certifier database
+conn = sqlite3.connect('files/attribute_certifier/attribute_certifier.db')
+x = conn.cursor()
 
 
 def generate_attributes():
@@ -28,18 +36,27 @@ def generate_attributes():
                                  'MECHANICS@TU']
     }
 
-    api = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
-    dict_users_dumped = json.dumps(dict_users)
-    name_file = 'files/users_attributes_' + str(process_instance_id) + '.txt'
-    with open(name_file, 'w') as ua:
-        ua.write('"process_instance_id": ' + str(process_instance_id) + '\n')
-        ua.write(dict_users_dumped)
-    new_file = api.add(name_file)
-    hash_file = new_file['Hash']
-    print(f'ipfs hash: {hash_file}')
+    f = io.StringIO()
+    f.write(str(dict_users))
+    f.seek(0)
+
+    file_to_str = f.read()
+
+    hash_file = api.add_json(file_to_str)
+    print(hash_file)
+
+    x.execute("INSERT OR IGNORE INTO user_attributes VALUES (?,?,?)", (process_instance_id, hash_file, file_to_str))
+    conn.commit()
 
     print(os.system('python3.11 blockchain/AttributeCertifierContract/AttributeCertifierContractMain.py %s %s %s %s' %
                     (certifier_private_key, app_id_certifier, process_instance_id, hash_file)))
+
+    # g = io.StringIO()
+    # g.write('ciao')
+    # g.seek(0)
+    # hash_file = api.add_json(g.read())
+    # print(os.system('python3.11 blockchain/AttributeCertifierContract/AttributeCertifierContractMain.py %s %s %s %s' %
+    #                 (certifier_private_key, app_id_certifier, 123, hash_file)))
 
 
 if __name__ == "__main__":

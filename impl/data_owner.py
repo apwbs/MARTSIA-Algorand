b2a481c8c580f42d5644f26817506281ff8537b5
apwbs <edoardo.marangone@uniroma1.it>
2023-01-07 17:11:33 +0100

    small fix

--- impl/data_owner.py
+++ impl/data_owner.py
@@ -54,7 +54,8 @@ def generate_pp_pk(process_instance_id):
     pk1 = api.cat(data[2])
     pk1 = pk1.decode('utf-8').rstrip('"').lstrip('"')
     pk1 = pk1.encode('utf-8')
-    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?)", (process_instance_id, 'Auth-1', pk1))
+    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?,?)",
+              (process_instance_id, 'Auth-1', data[2], pk1))
     conn.commit()
     # with open('files/data_owner/public_key_auth1_' + str(process_instance_id) + '.txt', 'wb') as ppw:
     #     ppw.write(pk1)
@@ -65,7 +66,8 @@ def generate_pp_pk(process_instance_id):
     pk2 = api.cat(data[2])
     pk2 = pk2.decode('utf-8').rstrip('"').lstrip('"')
     pk2 = pk2.encode('utf-8')
-    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?)", (process_instance_id, 'Auth-2', pk2))
+    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?,?)",
+              (process_instance_id, 'Auth-2', data[2], pk2))
     conn.commit()
 
     data = retrieve_data(authority3_address)
@@ -74,7 +76,8 @@ def generate_pp_pk(process_instance_id):
     pk3 = api.cat(data[2])
     pk3 = pk3.decode('utf-8').rstrip('"').lstrip('"')
     pk3 = pk3.encode('utf-8')
-    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?)", (process_instance_id, 'Auth-3', pk3))
+    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?,?)",
+              (process_instance_id, 'Auth-3', data[2], pk3))
     conn.commit()
 
     data = retrieve_data(authority4_address)
@@ -83,7 +86,8 @@ def generate_pp_pk(process_instance_id):
     pk4 = api.cat(data[2])
     pk4 = pk4.decode('utf-8').rstrip('"').lstrip('"')
     pk4 = pk4.encode('utf-8')
-    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?)", (process_instance_id, 'Auth-4', pk4))
+    x.execute("INSERT OR IGNORE INTO authorities_public_keys VALUES (?,?,?,?)",
+              (process_instance_id, 'Auth-4', data[2], pk4))
     conn.commit()
 
     # res = all(ele == check_parameters[0] for ele in check_parameters)  # another method to check if the list is equal
@@ -91,7 +95,8 @@ def generate_pp_pk(process_instance_id):
         getfile = api.cat(check_parameters[0])
         getfile = getfile.decode('utf-8').rstrip('"').lstrip('"')
         getfile = getfile.encode('utf-8')
-        x.execute("INSERT OR IGNORE INTO public_parameters VALUES (?,?)", (process_instance_id, getfile))
+        x.execute("INSERT OR IGNORE INTO public_parameters VALUES (?,?,?)",
+                  (process_instance_id, check_parameters[0], getfile))
         conn.commit()
         # with open('files/data_owner/public_parameters_dataowner_' + str(process_instance_id) + '.txt', 'wb') as ppw:
         #     ppw.write(getfile)
@@ -100,7 +105,7 @@ def generate_pp_pk(process_instance_id):
 def retrieve_public_parameters(process_instance_id):
     x.execute("SELECT * FROM public_parameters WHERE process_instance=?", (process_instance_id,))
     result = x.fetchall()
-    public_parameters = result[0][1]
+    public_parameters = result[0][2]
     # with open('files/data_owner/public_parameters_dataowner_' + str(process_instance_id) + '.txt', 'rb') as ppr:
     #     public_parameters = ppr.read()
     return public_parameters
@@ -121,25 +126,25 @@ def main(groupObj, maabe, api, process_instance_id):
     x.execute("SELECT * FROM authorities_public_keys WHERE process_instance=? AND authority_name=?",
               (process_instance_id, 'Auth-1'))
     result = x.fetchall()
-    pk1 = result[0][2]
+    pk1 = result[0][3]
     pk1 = bytesToObject(pk1, groupObj)
 
     x.execute("SELECT * FROM authorities_public_keys WHERE process_instance=? AND authority_name=?",
               (process_instance_id, 'Auth-2'))
     result = x.fetchall()
-    pk2 = result[0][2]
+    pk2 = result[0][3]
     pk2 = bytesToObject(pk2, groupObj)
 
     x.execute("SELECT * FROM authorities_public_keys WHERE process_instance=? AND authority_name=?",
               (process_instance_id, 'Auth-3'))
     result = x.fetchall()
-    pk3 = result[0][2]
+    pk3 = result[0][3]
     pk3 = bytesToObject(pk3, groupObj)
 
     x.execute("SELECT * FROM authorities_public_keys WHERE process_instance=? AND authority_name=?",
               (process_instance_id, 'Auth-4'))
     result = x.fetchall()
-    pk4 = result[0][2]
+    pk4 = result[0][3]
     pk4 = bytesToObject(pk4, groupObj)
 
     # public keys authorities
